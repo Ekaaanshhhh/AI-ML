03deedf54653fcc94668fb1757237a8b9a24af81
Ekansh Satsangi <ekanshsatsangi@gmail.com>
2025-12-27 11:55:30 +0530

    day 2 of ai/ml

---
+++ python_basics/strings.py
@@ -0,0 +1,12 @@
+a='ekansh'
+b='ekasnh'
+c=a+b#concatenation
+print(len(c));
+
+
+#slicing a string
+#taking parts out of a particular string
+#is called slicing
+str = "ekanshsatsangi"
+print(str[2:6]);#prints from index 2 to 5
+print(str[3:]);#prints sxtring from 3 to end
